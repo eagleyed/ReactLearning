8bbabbf39df8cb35c647ce7e488b27d9f1d24b08
icraftzone <rsgp.88@gmail.com>
2021-06-11 00:44:09 +0530

    React hook - useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,60 @@
-import React from "react";
-import { Counter } from "./features/counter/Counter"
+import React, { useState, useEffect } from "react";
 
 
 const App = () => {
+
+  const [apiData, setApiData] = useState([]);
+  //const [market, setMarket] = useState("Bangalore");
+
+  useEffect(() => {
+    fetch("https://api.data.gov.in/resource/9ef84268-d588-465a-a308-a864a43d0070?api-key=579b464db66ec23bdd0000012b6a45f5b99d4e715bdbd63991b2b141&format=json&offset=0&limit=100")
+    .then( response => response.json() )
+    .then( res => setApiData(res.records) )
+  }, [])
+
+  /**
+   * apiData fetching 100 records but we are filtering using array reduce / array filter + map.
+   */
+
+  const marketData = record =>
+    record.reduce((acc, item, index) => {
+      if( item.market.includes("Bangalore") ){
+        acc.push(
+        <li className="p-4 border border-indigo-600" key={index}>
+          <span className="block text-3xl">{item.commodity}</span>
+          <span className="block text-2xl">{item.market}</span>
+          <span className="block text-2xl">{item.district}</span>
+        </li>);
+      }
+      return acc;
+  }, []);
+
   return(
-    <div className="container">
-      <Counter />
+    <div className="container p-4">
+      <h1 className="text-center text-5xl font-bold p-4 mb-5 tracking-wide">
+        Market Daily Updates
+      </h1>
+      {/*}
+      <ul className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+      {apiData
+
+      .filter(item => item.market.includes("Bangalore"))
+      
+      .map((item, index) => (
+        <li className="p-4 border border-indigo-600" key={index}>
+          <span className="block text-3xl">{item.commodity}</span>
+          <span className="block text-2xl">{item.market}</span>
+          <span className="block text-2xl">{item.district}</span>
+        </li>
+      ))}
+      </ul>
+      {*/}
+    
+      {
+      <ul className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        {marketData(apiData)}
+      </ul>
+      }
     </div>
   )
 }
